59e749ce11086c2e6278a6e8a200f1da3a3964a0
Marcelly Marsura <marcellymarsura@gmail.com>
2025-01-13 10:54:07 -0300

    feat: Adicionando controller de produto e seed de produtos

---
+++ Ecommerce.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,101 @@
+﻿using Ecommerce.ProductAPI.Data.ValueObject;
+using Ecommerce.ProductAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> GetById(long id)
+        {
+            try
+            {
+                var product = await _repository.FindById(id);
+
+                if (product == null)
+                    return NotFound();
+
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ProductVO>>> GetAll()
+        {
+            try
+            {
+                var products = await _repository.FindAll();
+                return Ok(products);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Create([FromBody] ProductVO vo)
+        {
+            try
+            {
+                if (vo == null)
+                    return BadRequest();
+                var product = await _repository.Create(vo);
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update([FromBody] ProductVO vo)
+        {
+            try
+            {
+                if (vo == null)
+                    return BadRequest();
+
+                var product = await _repository.Update(vo);
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete(long id)
+        {
+            try
+            {
+                var result = await _repository.Delete(id);
+                if (!result)
+                    return BadRequest();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
